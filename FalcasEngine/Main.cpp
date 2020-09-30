580585df515efef4e37ec6dc8ee105858975bf50
Arnau77 <47742576+Arnau77@users.noreply.github.com>
2020-09-30 19:19:06 +0200

    add engine

---
+++ FalcasEngine/Main.cpp
@@ -0,0 +1,114 @@
+#include <stdlib.h>
+#include "Application.h"
+#include "Globals.h"
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_sdl.h"
+#include "ImGui/imgui_impl_opengl3.h"
+
+#include "SDL/include/SDL.h"
+#pragma comment( lib, "SDL/libx86/SDL2.lib" )
+#pragma comment( lib, "SDL/libx86/SDL2main.lib" )
+
+
+#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+#include <GL/gl3w.h>            // Initialize with gl3wInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+#include <GL/glew.h>            // Initialize with glewInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+#include <glad/glad.h>          // Initialize with gladLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
+#include <glad/gl.h>            // Initialize with gladLoadGL(...) or gladLoaderLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
+#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#include <glbinding/Binding.h>  // Initialize with glbinding::Binding::initialize()
+#include <glbinding/gl/gl.h>
+using namespace gl;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
+#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#include <glbinding/glbinding.h>// Initialize with glbinding::initialize()
+#include <glbinding/gl/gl.h>
+using namespace gl;
+#else
+//#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+#endif
+
+enum main_states
+{
+	MAIN_CREATION,
+	MAIN_START,
+	MAIN_UPDATE,
+	MAIN_FINISH,
+	MAIN_EXIT
+};
+
+int main(int argc, char ** argv)
+{
+	LOG("Starting game '%s'...", TITLE);
+
+	int main_return = EXIT_FAILURE;
+	main_states state = MAIN_CREATION;
+	Application* App = NULL;
+
+	while (state != MAIN_EXIT)
+	{
+		switch (state)
+		{
+		case MAIN_CREATION:
+
+			LOG("-------------- Application Creation --------------");
+			App = new Application();
+			state = MAIN_START;
+			break;
+
+		case MAIN_START:
+
+			LOG("-------------- Application Init --------------");
+			if (App->Init() == false)
+			{
+				LOG("Application Init exits with ERROR");
+				state = MAIN_EXIT;
+			}
+			else
+			{
+				state = MAIN_UPDATE;
+				LOG("-------------- Application Update --------------");
+			}
+
+			break;
+
+		case MAIN_UPDATE:
+		{
+			int update_return = App->Update();
+
+			if (update_return == UPDATE_ERROR)
+			{
+				LOG("Application Update exits with ERROR");
+				state = MAIN_EXIT;
+			}
+
+			if (update_return == UPDATE_STOP)
+				state = MAIN_FINISH;
+		}
+			break;
+
+		case MAIN_FINISH:
+
+			LOG("-------------- Application CleanUp --------------");
+			if (App->CleanUp() == false)
+			{
+				LOG("Application CleanUp exits with ERROR");
+			}
+			else
+				main_return = EXIT_SUCCESS;
+
+			state = MAIN_EXIT;
+
+			break;
+
+		}
+	}
+
+	delete App;
+	LOG("Exiting game '%s'...\n", TITLE);
+	return main_return;
+}
